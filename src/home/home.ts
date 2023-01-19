5cd236ecf490ac2b3e57e2b069a42f27dce3077f
Starlone <starlone.passos@gmail.com>
2023-01-19 10:23:56 -0300

    feat: loading, autorefresh

--- src/home/home.ts
+++ src/home/home.ts
@@ -4,18 +4,33 @@ import html from "./home.html";
 
 declare global { interface Window { service: any; } }
 
+function autoRefresh(comp: any) {
+    setTimeout(() => {
+        if (comp.hasAutorefresh) {
+            comp.onSubmit();
+        }
+        autoRefresh(comp);
+    }, 10000);
+}
+
 export const home = vue.defineComponent({
     name: "Hello",
     template: html,
     data() {
+        autoRefresh(this)
+        console.log(self);
         return {
             namespace: '',
-            pods: []
+            pods: [],
+            loading: false,
+            hasAutorefresh: false,
         }
     },
     methods: {
         async onSubmit() {
+            this.loading = true;
             const resp = await window.service.getPods(this.namespace);
+            this.loading = false;
             this.pods = resp.items;
             console.log(resp.items);
         },
